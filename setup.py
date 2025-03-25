7e1483fefa4e7ec0a5c163311bce6511837da018
Ben Young <ben.young@erg.com>
2025-03-25 16:02:37 -0400

    clean up dependencies, :bookmark: bump to v0.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="flcac_utils",
-    version="0.0.1",
+    version="0.1.0",
     packages=["flcac_utils"],
     include_package_data=True,
     python_requires=">=3.9",
     install_requires=["fedelemflowlist @ git+https://github.com/USEPA/fedelemflowlist.git@develop#egg=fedelemflowlist",
                       "esupy @ git+https://github.com/USEPA/esupy.git@develop#egg=esupy",
-                      "olca-schema>=2.2.2",
+                      "olca-schema>=2.2",
                       "bibtexparser<2.0",
                       "pandas>=2.2",
                       "numpy>=2.1",

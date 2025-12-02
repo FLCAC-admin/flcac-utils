3fb9219cc6abf74a901dd809894ea97204cb8bfc
Ben Young <ben.young@erg.com>
2025-12-02 13:52:59 -0500

    expands functions for preparing and applying technosphere flow mapping and building necessary bridge processes

--- flcac_utils/mapping.py
+++ flcac_utils/mapping.py
@@ -9,26 +9,46 @@ from esupy.util import make_uuid
 from flcac_utils.util import extract_flows, extract_processes
 
 def prepare_tech_flow_mappings(df, auth=False):
-    ## Identify mappings for technosphere flows (fuel inputs)
+    """
+    Prepares data objects from a technosphere flow mapping file
+
+    :param df: technosphere flow mapping file see format_specs/tech_mapping.md
+    :param auth: bool, if authorized access to FLCAC is required set to True
+
+    Returns:
+        flow_dict: dict where the key is the source flow name and the value is a
+            dictionary of data on the target flow with the following keys:
+                BRIDGE
+                name
+                provider
+                repo
+                conversion
+                unit
+                target_name
+                id
+        flow_objs: dict where the key is the soure flow name and the value is the
+            olca Flow object extracted from the FLCAC
+        provider_dict: dict where the key is the...
+    """
+    ## Identify mappings for technosphere flows
+    df = df.replace(np.nan, None)
     flow_dict = {row['SourceFlowName']:
-                      {'BRIDGE': row.get('Bridge'),
-                       'name': row['BridgeFlowName'] if row.get('BridgeFlowName') else row['TargetFlowName'],
-                       'provider': row.get('Provider') if not row.get('Bridge') else np.nan,
-                       'repo': {row['TargetRepoName']: row['TargetFlowName']},
-                       'conversion': row['ConversionFactor'],
-                       'unit': row['TargetUnit']} for _, row in df.iterrows()}
-                ## swap the flow names for bridge processes?
+        {'BRIDGE': False if pd.isna(row.get('Bridge')) else row.get('Bridge'),
+         'target_name': row['BridgeFlowName'] if row.get('BridgeFlowName') else row['TargetFlowName'],
+         'name': row['TargetFlowName'],
+         'provider': row.get('Provider') if not row.get('Bridge') else np.nan,
+         'repo': np.nan if pd.isna(row['TargetRepoName']) else {row['TargetRepoName']: row['TargetFlowName']},
+         'conversion': row['ConversionFactor'],
+         'unit': row['TargetUnit']} for _, row in df.iterrows()}
+        ## swap the flow names for bridge processes?
     
-    ## extract fuel objects in flow_dict from commons via API
+    ## extract flow objects in flow_dict from commons via API
     f_dict = {}
     p_dict = {}
     for k, v in flow_dict.items():
-        if 'repo' in v:
+        if ('repo' in v) and (not pd.isna(v.get('repo'))):
             repo = list(v.get('repo').keys())[0]
             flow = list(v.get('repo').values())[0]
-            flow_dict[k]['target_name'] = flow
-            if not flow_dict[k].get('BRIDGE'):
-                flow_dict[k]['name'] = flow
             if repo in f_dict:
                 f_dict[repo].extend([flow])
             else:
@@ -47,8 +67,147 @@ def prepare_tech_flow_mappings(df, auth=False):
             o = flow_objs.get(v['name'])
             flow_dict[k]['id'] = o.id if o else None
             if not o:
-                print(f'Flow: {v["name"]} not found.')
+                if pd.isna(flow_dict[k]['repo']):
+                    print(f'New flow needed: {v["name"]}.')
+                else:
+                    print(f'Flow: {v["name"]} not found.')
         else:
             flow_dict[k]['id'] = make_uuid(flow_dict[k].get('name'))
+            ## TODO: double check if this should be target_name and not name?
     
     return (flow_dict, flow_objs, provider_dict)
+
+def apply_tech_flow_mapping(df, flow_dict, flow_objs, provider_dict, cond=None) -> pd.DataFrame:
+    """
+    Updates the dataframe to implement the tech flow mapping.
+    Input data frame must have:
+        'name' --> SourceFlowName
+        'amount' --> Source amount
+    pass condition if desired, e.g., cond = df['FlowName'] != "Not this flow"
+    """
+    if 'FlowUUID' not in df:
+        df['FlowUUID'] = np.nan
+    if 'name' not in df:
+        raise KeyError("'name' must be in passed dataframe")
+    if 'amount' not in df:
+        raise KeyError("'amount' must be in passed dataframe")
+    if 'unit' not in df:
+        raise KeyError("'unit' must be in passed dataframe")
+
+    if not cond:
+        cond = df['FlowType'] != "ELEMENTARY_FLOW"
+
+    def get_context(n):
+        try:
+            return flow_objs.get(n).category
+        except AttributeError:
+            return ''
+
+    df = (df
+          ## Flow mapping and conversions
+          .assign(FlowUUID = lambda x: np.where(cond,
+                x['name'].map(
+                    {k: v['id'] for k, v in flow_dict.items()}), 
+                x['FlowUUID']))
+          .assign(FlowName = lambda x: np.where(cond,
+                x['name'].map(
+                    {k: v['name'] for k, v in flow_dict.items()}),
+                x['name']))
+          .assign(Context = lambda x: np.where(cond,
+                x['name'].map(
+                    {k: get_context(v['target_name']) for k, v in flow_dict.items()}),
+                x['Context']))
+           .assign(unit = lambda x: np.where(cond, 
+                x['name'].map(
+                    {k: v.get('unit') for k, v in flow_dict.items()}),
+                x['unit']))
+           .assign(conversion = lambda x: np.where(cond, 
+                x['name'].map(
+                    {k: v.get('conversion', 1) for k, v in flow_dict.items()}),
+                1))
+           .assign(Flowamount = lambda x: x['amount'] * x['conversion'])
+
+        ## Handle default providers
+           .assign(default_provider_process = lambda x: np.where(cond,
+                x['name'].map(
+                    {k: v['provider'] for k, v in flow_dict.items() 
+                     if not pd.isna(v['provider'])}),
+                ''))
+           .assign(default_provider = lambda x: np.where(cond,
+                x['default_provider_process'].map(
+                    {k: v.id for k, v in provider_dict.items()}),
+                ''))
+
+        ## Handle bridge processes
+           .assign(bridge = lambda x: np.where(cond,
+               x['name'].map(
+                   {k: True for k, v in flow_dict.items()
+                    if v.get('BRIDGE', False)}),
+               False))
+           .assign(repo = lambda x: np.where(cond,
+               x['name'].map(
+                   {k: list(v['repo'].keys())[0] for k, v in flow_dict.items()
+                    if v.get('BRIDGE', False)}),
+               ''))
+           )
+
+    return df
+
+def create_bridge_name(repo, flowname):
+    if repo == 'USLCI':
+        return f'{flowname} PROXY'
+    else:
+        return f'{flowname} BRIDGE, USLCI to {repo}'
+
+def create_bridge_category(repo, flowname):
+    if repo == 'USLCI':
+        return 'Bridge Processes'
+    else:
+        return f'Bridge Processes / USLCI to {repo}'
+
+
+def create_bridge_processes(df, flow_dict, flow_objs):
+    """ Builds bridge processes to facilitate technosphere flow mapping"""
+    flow_dict1 = {k: v for k, v in flow_dict.items() if v.get('BRIDGE', False)}
+    if len(df.query('bridge == True')) == 0:
+        return pd.DataFrame()
+    df_bridge = (df
+                 .query('bridge == True')
+                 .drop_duplicates(subset = 'FlowName')
+                 .reset_index(drop=True)
+                 .assign(amount = 1)
+                 .assign(ProcessCategory = lambda x: x.apply(
+                     lambda z: create_bridge_category(z['repo'], z['FlowName']), axis=1))
+                 .assign(ProcessName = lambda x: x.apply(
+                     lambda z: create_bridge_name(z['repo'], z['FlowName']), axis=1))
+                 .assign(ProcessID = lambda x: x['ProcessName'].apply(make_uuid))
+                 # ^ need more args passed to UUID to avoid duplicates?
+                 )
+    df_bridge = (pd.concat([
+            df_bridge
+                .assign(reference = lambda x: ~x['reference'])
+                .assign(IsInput = lambda x: ~x['IsInput'])
+                .assign(FlowUUID = lambda x: x['FlowName'].apply(make_uuid)),
+            # ^ first chunk is for new flows
+            df_bridge
+               .assign(FlowName = lambda x: x['name'].map(
+                   {k: v.get('target_name', 'ERROR') for k, v in flow_dict1.items()}))
+               .assign(FlowUUID = lambda x: x['name'].map(
+                   {k: flow_objs.get(v['name']).id for k, v in flow_dict1.items()}))
+               .assign(unit = lambda x: x['name'].map(
+                   {k: v.get('unit') for k, v in flow_dict1.items()}))
+               .assign(conversion = lambda x: x['name'].map(
+                   {k: v.get('conversion', 1) for k, v in flow_dict1.items()}))
+               .assign(amount = lambda x: x['amount'] * x['conversion'])
+               # ^ apply unit conversion
+               .drop(columns=['conversion'])
+               .assign(Tag = lambda x: x['name'].map(
+                   {k: list(v.get('repo').keys())[0] for k, v in flow_dict1.items()}))
+             # ^ second chunk is for bridged flows
+
+             ## TODO Need to add default providers for these when they are bridges WITHIN
+             # a database? Would be nice, but not required
+            ], ignore_index=True)
+            .drop(columns=['bridge'])
+        )
+    return df_bridge
---
+++ format_specs/tech_mapping.md
@@ -0,0 +1,21 @@
+# Format specifications for technosphere flow mapping file
+
+| Field             | Type  | Required | Note                                                                                 |
+|-------------------|-------|----------|--------------------------------------------------------------------------------------|
+| SourceRepoName    |       | N        |                                                                                      |
+| SourceFlowName    | str   | Y        | Flowname in the original dataset                                                     |
+| SourceFlowUUID    |       | N        |                                                                                      |
+| SourceFlowContext |       | N        |                                                                                      |
+| SourceUnit        | str   | Y        | Unit in the original dataset                                                         |
+| MatchCondition    |       |          |                                                                                      |
+| ConversionFactor  | float | N        | Quantity to convert SourceUnit to TargetUnit, default is 1                           |
+| TargetRepoName    | str   | Y        | Target repo name, if blank a new flow will be created                                |
+| TargetFlowName    | str   | Y        | Target flow name, must match flow name exactly for existing flows                    |
+| TargetUnit        | str   | Y        |                                                                                      |
+| Provider          |       |          | Process Name of default provider if desired, if Bridge is `True`, this is not needed |
+| Bridge            | bool  | N        | If `True`, will create a bridge process from BridgeFlowName to TargetFlowName        |
+| BridgeFlowName    | str   | N        |                                                                                      |
+| Mapper            |       | N        |                                                                                      |
+| Verifier          |       | N        |                                                                                      |
+| LastUpdated       |       | N        |                                                                                      |
+| ConversionSource  |       | N        | If a conversion is needed, indicate the source for conversion factor                 |

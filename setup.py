b9eec300394a46e69ae2bee71da70f117e409a4b
Ben Young <ben.young@erg.com>
2025-12-15 17:27:38 -0500

    :bookmark: bump to v0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="flcac_utils",
-    version="0.2.1",
+    version="0.2.2",
     packages=["flcac_utils"],
     include_package_data=True,
     python_requires=">=3.9",
